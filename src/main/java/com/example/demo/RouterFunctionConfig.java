95f30ef5c0273e39a216f8ff01bb346cf2450327
SUHOPARK1 <qkrtnghsla@naver.com>
2021-01-27 16:49:53 +0900

    React 와 connect 완료

--- src/main/java/com/example/demo/RouterFunctionConfig.java
+++ src/main/java/com/example/demo/RouterFunctionConfig.java
@@ -1,24 +1,34 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Handles requests for the application home page.
  */
+@CrossOrigin(origins="*")
 @RestController
 public class RouterFunctionConfig {
-/*
-    @Bean
-    public RouterFunction<?> index() {
-        return route(GET("/hello"),
-                request -> ok().body(just("Hello World !!"), String.class));
-    }
+    /*
+        @Bean
+        public RouterFunction<?> index() {
+            return route(GET("/hello"),
+                    request -> ok().body(just("Hello World !!"), String.class));
+        }
 
-*/
-    @GetMapping("/hello")
-    public String hello(){
-        System.out.println("React 와 Boot 서버 연결");
-        return "Hello Boot !!";
+    */
+    @GetMapping("/test")
+    public Map<?,?> hello(){
+        System.out.println("React Boot connect Success");
+        Map<String,String> map = new HashMap<>();
+        map.put("a","a");
+        map.put("b","b");
+        return map;
     }
 }
+
+
