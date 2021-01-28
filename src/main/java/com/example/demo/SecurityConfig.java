303ae3ce1d207425c6e7ce000f2b10e4ce234fda
SUHOPARK1 <qkrtnghsla@naver.com>
2021-01-28 10:44:04 +0900

    React 와 connect 완료 - consol access

--- src/main/java/com/example/demo/SecurityConfig.java
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -35,7 +35,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 )
                 .and()
                 .httpBasic();
-                
     }
 
     @Override
